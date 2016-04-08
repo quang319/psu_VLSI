d835b3982c2459ada6b277826b96a635f5f43001
QuangNguyen <quang319@gmail.com>
2016-04-07 21:40:41 -0400

    add LUTs

--- project/Phase 2/CGenLUT/CGenLUT/CGenLUT/Program.cs
+++ project/Phase 2/CGenLUT/CGenLUT/CGenLUT/Program.cs
@@ -30,34 +30,59 @@ namespace CGenLUT
         {
             List<short> listCr = new List<short>();
             List<short> listCb = new List<short>();
-            List<double> listCr_exp = new List<double>();
-            List<double> listCb_exp = new List<double>();
-            short resultCr;
-            short resultCb;
+            List<short> listWcr = new List<short>();
+            List<short> listWcb = new List<short>();
+
+            //List<double> listCr_exp = new List<double>();
+            //List<double> listCb_exp = new List<double>();
+            //short resultCr;
+            //short resultCb;
             for (int i = 0; i < 256; i++)
             {
-                double CrTemp = MeanCr(i);
-                double CbTemp = MeanCb(i);
-                resultCr = doubleToShort(CrTemp);
-                resultCb = doubleToShort(CbTemp);
-                listCr_exp.Add(CrTemp);
-                listCb_exp.Add(CbTemp);
-                listCr.Add(resultCr);
-                listCb.Add(resultCb);
-            }
+                //double CrTemp = MeanCr(i);
+                //double CbTemp = MeanCb(i);
+                //resultCr = doubleToShort(CrTemp);
+                //resultCb = doubleToShort(CbTemp);
+                //listCr_exp.Add(CrTemp);
+                //listCb_exp.Add(CbTemp);
+                //listCr.Add(resultCr);
+                //listCb.Add(resultCb);
 
-            System.IO.File.WriteAllText("MeanCr_LUT.list", string.Empty);
-            using (StreamWriter writer = new StreamWriter("MeanCr_LUT.list"))
-            {
-                foreach(short item in listCr)
-                {
-                    //writer.WriteLine(Convert.ToString(item,2).PadLeft(16,'0'));
-                    writer.WriteLine(item.ToString("X").PadLeft(4, '0'));
-                }
+                listCr.Add( doubleToShort( MeanCr(i) ) );
+                listCb.Add( doubleToShort( MeanCb(i) ) );
+
+                listWcr.Add( doubleToShort( WidthCr(i) ) );
+                listWcb.Add( doubleToShort( WidthCb(i) ) );
             }
+            clearAndWriteToFile("MeanCr_LUT.list", listCr);
+            clearAndWriteToFile("MeanCb_LUT.list", listCb);
+            clearAndWriteToFile("WidthCr_LUT.list", listWcr);
+            clearAndWriteToFile("WidthCb_LUT.list", listWcb);
+
+
         }
 
         static double MeanCr(int Y)
+        {
+            if (Y <= K_l)
+                return 154.0 + 10.0 * (K_l - Y) / (K_l - Y_min);
+            else if (K_h <= Y)
+                return 154.0 + 22.0 * (Y - K_h) / (Y_max - K_h);
+            else
+                return 0.0;
+        }
+
+        static double MeanCb(int Y)
+        {
+            if (Y <= K_l)
+                return 108.0 + 10.0 * (K_l - Y) / (K_l - Y_min);
+            else if (K_h <= Y)
+                return 108.0 + 10.0 * (Y - K_h) / (Y_max - K_h);
+            else
+                return 0.0;
+        }
+
+        static double WidthCr(int Y)
         {
             if (Y <= K_l)
             {
@@ -71,19 +96,37 @@ namespace CGenLUT
                 return 0.0;
         }
 
-        static double MeanCb(int Y)
+        static double WidthCb(int Y)
         {
             if (Y <= K_l)
+            {
                 return WL_Cb + (Y - Y_min) * (W_Cb - WL_Cb) / (K_l - Y_min);
+            }
             else if (K_h <= Y)
+            {
                 return WH_Cb + (Y_max - Y) * (W_Cb - WH_Cb) / (Y_max - K_h);
+            }
             else
                 return 0.0;
         }
 
+        // Note: we are using Q7 notation here      
         static short doubleToShort(double input)
         {
-            return Convert.ToInt16(input * 256.0);
+            return Convert.ToInt16(input * 128.0);
+        }
+
+        static void clearAndWriteToFile(string fileName, List<short> inputList)
+        {
+            System.IO.File.WriteAllText(fileName, string.Empty);
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (short item in inputList)
+                {
+                    //writer.WriteLine(Convert.ToString(item,2).PadLeft(16,'0'));
+                    writer.WriteLine(item.ToString("X").PadLeft(4, '0'));
+                }
+            }
         }
 
     }
