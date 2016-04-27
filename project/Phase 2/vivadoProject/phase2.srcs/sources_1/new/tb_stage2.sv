ca016778d3a9f048051a470cf1997999898d5875
QuangNguyen <quang319@gmail.com>
2016-04-27 14:23:45 -0400

    testing tb_stage2

--- project/Phase 2/vivadoProject/phase2.srcs/sources_1/new/tb_stage2.sv
+++ project/Phase 2/vivadoProject/phase2.srcs/sources_1/new/tb_stage2.sv
@@ -25,14 +25,41 @@ module tb_stage2;
 	reg signed [31:0] rTransCb;
 	reg signed [31:0] rTransCr;
 
-	wire signed [31:0] leftOutput;
-	wire signed [31:0] rightOutput;
+	wire signed [31:0] wLeftOutput;
+	wire signed [31:0] wRightOutput;
+
+	// reg signed [31:0] rLeftOutput;
+	// reg signed [31:0] rRightOutput;
+
+	int i;
 
 	stage2 uut_st2 (
 		.clk(clk),
 		.rTransCb(rTransCb),
 		.rTransCr(rTransCr),
-		.leftOutput(leftOutput),
-		.rightOutput(rightOutput)
+		.leftOutput(wLeftOutput),
+		.rightOutput(wRightOutput)
 		);
+
+	always #5 clk =~ clk;
+
+	// Set clk value 
+	intitial begin
+		clk = 0;
+	end
+
+	initial begin 
+		f = $fopen("stage2_output.txt","w");
+		for (i = 0; i < 20; i = i + 1 ) begin
+			@(posedge clk);
+			rTransCb <= 32d'2469953;
+			rTransCb <= 32d'1749319;
+			// rLeftOutput <= wLeftOutput;
+			// rRightOutput <= wRightOutput;
+			$display("%d, %d, %d\n",i, wLeftOutput, wRightOutput);
+			$fwrite("%d, %d, %d\n",i, wLeftOutput, wRightOutput);
+		end
+		$fclose(f);
+		$finish;
+	end
 endmodule
