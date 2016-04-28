fa549b385d3bcd1d2ce83248b53a37ed1ed8b5d9
QuangNguyen <quang319@gmail.com>
2016-04-28 15:29:38 -0400

    formated valid

--- project/Phase 2/python_testCode/generateAllSkinscores.py
+++ project/Phase 2/python_testCode/generateAllSkinscores.py
@@ -7,7 +7,14 @@ def extractNPrintFromCSV (fileName):
 	[print('8\'d%s,' %(data)) for data in dataList]
 	return dataList
 
+def genValids (number):
+	dataList = range(0,286)
+	[print('8\'d%s,' %(1)) for data in dataList]
+	return dataList
+
+
 # dataList = extractNPrintFromCSV('PossibleCr.csv')
 # dataList = extractNPrintFromCSV('PossibleCb.csv')
-dataList = extractNPrintFromCSV('PossibleY.csv')
+# dataList = extractNPrintFromCSV('PossibleY.csv')
+dataList = genValids(686)
 print(len(dataList))
